fdb4f33d49a4209597936ee05dc1acc6f84fc637
David Thomason <dlthomas@us.ibm.com>
2018-09-21 13:45:33 -0400

    renamed todoTasksPresent to tasksPresent, passing in list to check

--- tests/e2e/test.js
+++ tests/e2e/test.js
@@ -18,11 +18,11 @@ fixture `To Do List`
   const doneTasks = doneList.find('.task')
   const doneCheckboxes = doneTasks.find('input').withAttribute('type', 'checkbox')
 
-  const todoTasksPresent = ClientFunction(expectedTasks => {
-    const visibleTasks = getTodoList().childNodes
-    return visibleTasks.length === expectedTasks.length &&
-      [].every.call(visibleTasks, (task, i) => task.textContent.includes(expectedTasks[i]))
-  },{ dependencies: { getTodoList: todoList } });
+  const tasksPresent = ClientFunction((taskList, expectedTasks) => {
+    const tasks = taskList().childNodes
+    return tasks.length === expectedTasks.length &&
+      [].every.call(tasks, (task, i) => task.textContent.includes(expectedTasks[i]))
+  });
 
 //then create a test and place your code there
 test('My first test', async t => {
@@ -32,30 +32,31 @@ test('My first test', async t => {
     .expect(todoTasks.count).eql(0)
 
     .typeText(newTaskInput, task1).pressKey('enter')
-    .expect(todoTasksPresent([task1])).ok()
+    .expect(tasksPresent(todoList, [task1])).ok()
     .expect(todoCheckboxes.count).eql(1)
     .expect(todoCheckboxes.nth(0).checked).notOk()
 
     .typeText(newTaskInput, task2).pressKey('enter')
-    .expect(todoTasksPresent([task1, task2])).ok()
+    .expect(tasksPresent(todoList, [task1, task2])).ok()
     .expect(todoCheckboxes.count).eql(2)
     .expect(todoCheckboxes.nth(0).checked).notOk()
     .expect(todoCheckboxes.nth(1).checked).notOk()
 
     .typeText(newTaskInput, task3).pressKey('enter')
-    .expect(todoTasksPresent([task1, task2, task3])).ok()
+    .expect(tasksPresent(todoList, [task1, task2, task3])).ok()
     .expect(todoCheckboxes.count).eql(3)
     .expect(todoCheckboxes.nth(0).checked).notOk()
     .expect(todoCheckboxes.nth(1).checked).notOk()
     .expect(todoCheckboxes.nth(2).checked).notOk()
 
     .click(todoTasks.withText(task2).find('input').withAttribute('type', 'checkbox'))
-    .expect(todoTasksPresent([task1, task3])).ok()
+    .expect(tasksPresent(todoList, [task1, task3])).ok()
     .expect(todoCheckboxes.count).eql(2)
     .expect(todoCheckboxes.nth(0).checked).notOk()
     .expect(todoCheckboxes.nth(1).checked).notOk()
     .expect(doneList.child('h3').innerText).eql('Completed Tasks')
     .expect(doneTasks.count).eql(1)
+    .expect(tasksPresent(doneTasks, [task2])).ok()
     .expect(doneCheckboxes.count).eql(1)
     .expect(doneCheckboxes.nth(0).checked).ok()
 });
