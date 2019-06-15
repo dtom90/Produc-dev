b3be61439da087035727d16fb73bce4030a7bac8
David Thomason <dtom90@users.noreply.github.com>
2019-06-15 12:15:44 -0400

    Corrected and updated tests/unit/TaskList.spec.js

--- tests/unit/TaskList.spec.js
+++ tests/unit/TaskList.spec.js
@@ -1,27 +1,64 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import TaskList from '@/components/TaskList.vue'
+
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import {library} from "@fortawesome/fontawesome-svg-core";
+import {faCog, faEllipsisH, faPencilAlt, faPlay, faSave, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
+library.add(faTrashAlt, faPlay, faSave, faCog, faEllipsisH, faPencilAlt)
 
 const localVue = createLocalVue();
 localVue.component('font-awesome-icon', FontAwesomeIcon)
 
-import TaskList from '@/components/TaskList.vue'
-
 describe('TaskList.vue', () => {
   
-  it('renders props.name when passed', () => {
-    const tasks = [
-      {id: 1, name: 'new task 1'},
-      {id: 2, name: 'new task 2'},
-      {id: 3, name: 'new task 3'}
-    ]
-    const wrapper = shallowMount(TaskList, {
+  const tasks = [
+    {id: 1, name: 'new task 1'},
+    {id: 2, name: 'new task 2'},
+    {id: 3, name: 'new task 3'}
+  ]
+  let wrapper;
+  
+  beforeEach(() => {
+  
+    wrapper = mount(TaskList, {
       propsData: { tasks: tasks },
       localVue
     })
+    
+  })
+  
+  it('should have tasks loade into props', () => {
+    
+    expect(wrapper.props().tasks).toBe(tasks)
+  
+  })
+  
+  it('renders tasks in oldest-first order', () => {
+  
+    wrapper.setData({
+      sortOrder: 'Oldest'
+    })
+  
+    expect(wrapper.vm.sortOrder).toBe('Oldest')
+    
     const renderedTasks = wrapper.findAll('li')
     renderedTasks.wrappers.forEach((elem, i) => {
-      expect(elem.text()).toMatch(tasks[i].name)
+      expect(elem.text()).toMatch(tasks[i].name)//'fasdsa'
     })
+    
+  })
+  
+  it('renders tasks in newest-first order', () => {
+  
+    wrapper.setData({
+      sortOrder: 'Newest'
+    })
+    
+    const renderedTasks = wrapper.findAll('li')
+    renderedTasks.wrappers.forEach((elem, i) => {
+      expect(elem.text()).toMatch(tasks[tasks.length-1-i].name)
+    })
+    
   })
   
 })
