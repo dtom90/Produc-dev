c98ee5b6f045bee3976452320f98eda60480e450
David Thomason <dtom90@users.noreply.github.com>
2019-07-18 22:07:24 +0200

    Testing that Task selects the task when clicked

--- tests/unit/Task.spec.js
+++ tests/unit/Task.spec.js
@@ -1,27 +1,46 @@
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Task from '@/components/Task.vue'
-import store from '@/store'
-
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Vuex from 'vuex'
 
 const localVue = createLocalVue()
 localVue.component('font-awesome-icon', FontAwesomeIcon)
+localVue.use(Vuex)
+
+const mutations = {
+  selectTask: jest.fn()
+}
+
+const store = new Vuex.Store({
+  mutations
+})
+
+const taskName = 'new task 1'
+const task = { id: 1, name: taskName }
+let wrapper
 
 describe('Task', () => {
   
-  it('renders renders the task name', () => {
-    
-    const taskName = 'new task 1'
-    const task = { id: 1, name: taskName }
-    
-    const wrapper = shallowMount(Task, {
+  beforeEach(() => {
+    wrapper = shallowMount(Task, {
       propsData: { task: task },
-      store,
-      localVue
+      localVue,
+      store
     })
+  })
+  
+  it('renders renders the task name', () => {
     
     const li = wrapper.find('li')
     expect(li.text()).toMatch(taskName)
+    
+  })
+  
+  it('selects the task when clicked', () => {
+  
+    wrapper.trigger('click')
+    expect(mutations.selectTask).toHaveBeenCalled()
+    
   })
   
 })
