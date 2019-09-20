a0a3ccf1f415c05ee0e3984fbe38b810f55f765b
David Thomason <dtom90@users.noreply.github.com>
2019-09-20 08:45:46 -0400

    Updated web/electron structure

---
+++ electron.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+let url
+if (process.env.NODE_ENV === 'DEV') {
+  url = 'http://localhost:8080/'
+} else {
+  url = `file://${process.cwd()}/dist-electron/index.html`
+}
+
+function createWindow () {
+  win = new BrowserWindow({ width: 1000, height: 800 })
+  
+  win.loadURL(url)
+  
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
--- web.js
+++ web.js
@@ -5,7 +5,7 @@ const app = express()
 const port = 5000
 const baseUrl = process.env.BASE_URL || '/'
 
-app.use(baseUrl, express.static(path.join(__dirname, 'dist')))
+app.use(baseUrl, express.static(path.join(__dirname, 'dist-web')))
 
 // eslint-disable-next-line no-console
-app.listen(port, () => console.log(`Produc-dev listening on port ${port}!`))
+app.listen(port, () => console.log(`Serving Produc-dev at http://localhost:${port}${baseUrl}`))
