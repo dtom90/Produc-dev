ad288c04de58286468b2b3c0a482bda7b0af7044
David Thomason <dtom90@users.noreply.github.com>
2019-09-25 08:03:44 -0400

    Changed standard electron build path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ module.exports = {
   publicPath: process.env.BASE_URL
     ? process.env.BASE_URL
     : process.env.BUILD_ENV === 'electron'
-      ? `${process.cwd()}/dist-electron/`
+      ? `${process.cwd()}/dist_electron/build/`
       : '/'
 }
