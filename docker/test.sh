26e78adafee3102c5c4e3af54c3b1acbd778c400
David Thomason <dtom90@users.noreply.github.com>
2019-10-29 18:47:00 -0400

    Updated Docker files. test.sh almost working

--- docker/test.sh
+++ docker/test.sh
@@ -1,29 +1,28 @@
 #!/usr/bin/env bash
 
 THIS_DIR=$(dirname "$0")
-cd "${THIS_DIR}/.."
+cd "${THIS_DIR}/.." || exit
+
+IMAGE_NAME=producdev-prod
+CONTAINER_NAME=producdev-prod
 
 echo
-echo "Building test container..."
+echo "Running Lint and Unit Tests..."
 echo
-docker build \
-       -f docker/Dockerfile.dev \
-       -t produc-dev-test \
-       . && \
-echo && echo "Running Lint and Unit Tests..." && echo &&\
-docker run -it --rm \
-       --name produc-dev-test \
-       produc-dev-test \
-       sh -c 'yarn run lint && yarn run test:unit'
+./docker/temp.sh yarn run lint && yarn run test:unit
 test_exit_code=$?
 if [ ${test_exit_code} != 0 ]; then exit ${test_exit_code}; fi
 echo
 echo "Deploying production container..."
 echo
+docker build \
+       -f docker/Dockerfile.prod \
+       -t ${IMAGE_NAME} \
+       . && \
 docker run -d --rm \
        -p 8080:80 \
-       --name produc-dev-prod \
-       produc-dev-prod
+       --name ${CONTAINER_NAME} \
+       ${IMAGE_NAME}
 test_exit_code=$?
 if [ ${test_exit_code} != 0 ]; then exit ${test_exit_code}; fi
 docker run --rm \
@@ -32,19 +31,19 @@ docker run --rm \
            --retry 5 --retry-connrefused \
            http://localhost:8080
 test_exit_code=$?
-if [ ${test_exit_code} != 0 ]; then docker stop produc-dev-prod && exit ${test_exit_code}; fi
+if [ ${test_exit_code} != 0 ]; then docker stop ${CONTAINER_NAME} && exit ${test_exit_code}; fi
 echo
 echo "Production container deployed."
 echo
 echo "Testing against production container..."
 echo
 docker run -it --rm \
-           -v `pwd`/tests/e2e:/tests \
            --net="host" \
-           testcafe/testcafe chromium,firefox /tests
+           producdev-temp \
+           ./node_modules/.bin/testcafe chrome:headless tests/e2e -c 4 --selector-timeout 3000
 test_exit_code=$?
 echo
 echo "Stopping production container..."
 echo
-docker stop produc-dev-prod
+docker stop ${CONTAINER_NAME}
 exit ${test_exit_code}
