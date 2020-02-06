f5db289a9d4f750e234b40df762ddad1bc3559b5
David Thomason <dtom90@users.noreply.github.com>
2020-02-06 10:48:40 -0500

    Added Navbar toggler

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,24 +1,41 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-sm navbar-light bg-light">
     <span class="navbar-brand mb-0 h1">DevTrack</span>
-    <ul class="navbar-nav ml-auto">
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          data-toggle="modal"
-          data-target="#tagModal"
-          href="javascript:void(0);"
-        >Tags</a>
-      </li>
-      <li class="nav-item">
-        <a
-          class="nav-link"
-          data-toggle="modal"
-          data-target="#dataModal"
-          href="javascript:void(0);"
-        >Data</a>
-      </li>
-    </ul>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarMenuOptions"
+      aria-controls="navbarMenuOptions"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon" />
+    </button>
+
+    <div
+      id="navbarMenuOptions"
+      class="collapse navbar-collapse"
+    >
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            data-toggle="modal"
+            data-target="#tagModal"
+            href="javascript:void(0);"
+          >Tags</a>
+        </li>
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            data-toggle="modal"
+            data-target="#dataModal"
+            href="javascript:void(0);"
+          >Data</a>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
