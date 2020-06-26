ecfae327643ad168a25391ea66ede03918164c82
David Thomason <dtom90@users.noreply.github.com>
2020-06-26 08:38:02 -0400

    Revert notes horizontal scroll, break word instead

--- src/components/SelectedTask.vue
+++ src/components/SelectedTask.vue
@@ -115,6 +115,7 @@
     <div
       id="notes-section"
       class="d-flex align-items-center"
+      style="max-width: 100%"
     >
       <span id="notes-label">Notes: </span>
       
@@ -123,7 +124,6 @@
       <span
         v-if="task.notes && !editingNotes"
         id="display-notes"
-        class="flex-grow-1"
         v-html="displayNotes"
       />
       <!-- eslint-enable vue/no-v-html -->
@@ -144,7 +144,7 @@
       >
         <textarea
           v-model="task.notes"
-          class="form-control no-wrap"
+          class="form-control"
           :rows="task.notes.split('\n').length"
         />
         <div class="input-group-append">
@@ -207,6 +207,10 @@ import ActivityView from './ActivityView'
 import { mapMutations, mapState } from 'vuex'
 import marked from 'marked'
 import DOMPurify from 'dompurify'
+
+marked.setOptions({
+  breaks: true
+})
 
 const renderer = new marked.Renderer()
 const linkRenderer = renderer.link
@@ -376,17 +380,15 @@ export default {
   padding-right: 15px;
 }
 
-.no-wrap {
-  white-space: nowrap;
-}
-
 #display-notes {
   padding: 10px;
   border: #e2e6ea 1px solid;
   border-radius: 5px;
   font-size: 18px;
-  overflow-x: scroll;
-  @extend .no-wrap;
+  flex: 1;
+  min-width:0;
+  overflow: visible;
+  overflow-wrap: break-word;
 }
 
 .dropdown .btn {
