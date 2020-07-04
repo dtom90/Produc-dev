c16f9382ff861c04c174b132d00120a3d70c8498
David Thomason <dtom90@users.noreply.github.com>
2020-07-04 11:40:31 -0400

    Moved config copy into container

--- docker/gh-pages.sh
+++ docker/gh-pages.sh
@@ -19,6 +19,6 @@ docker run -it \
        -p 8080:8080 \
        --name ${CONTAINER_NAME} \
        ${IMAGE_NAME} \
-       yarn run gh_pages:build && \
+       sh -c 'yarn run gh_pages:build && cp -r .circleci dist_gh_pages' && \
 docker cp ${CONTAINER_NAME}:/app/dist_gh_pages ./dist_gh_pages && \
 docker rm -f ${CONTAINER_NAME}
