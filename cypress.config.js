e571f8f1d0e547635ad8e6d132ce5932605f55cc
David Thomason <dtom90@users.noreply.github.com>
2024-11-12 13:08:28 -0500

    Fix cypress

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents (on, config) {
       // implement node event listeners here
       on('task', {
-        log(message) {
+        log (message) {
+          // eslint-disable-next-line no-console
           console.log(message)
           return null
         }
