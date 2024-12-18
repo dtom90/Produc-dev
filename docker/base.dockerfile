695bde2f059401d0f6c4e849666fa36ba60ca897
David Thomason <dtom90@users.noreply.github.com>
2024-12-18 11:30:24 -0500

    Fix dependencies

--- docker/base.dockerfile
+++ docker/base.dockerfile
@@ -1,4 +1,4 @@
-FROM node:18.20.5-alpine
+FROM node:16.20.2-alpine
 
 RUN apk update && apk upgrade && \
     apk add --no-cache bash git openssh
--- docker/test_basic.sh
+++ docker/test_basic.sh
@@ -3,6 +3,6 @@
 THIS_DIR=$(dirname "$0")
 cd "${THIS_DIR}/.." || exit
 
-./docker/temp.sh sh -c "yarn audit:packages && yarn run lint && yarn run test:unit"
+./docker/temp.sh sh -c "yarn packages:audit && yarn run lint && yarn run test:unit"
 test_exit_code=$?
 if [ ${test_exit_code} != 0 ]; then exit ${test_exit_code}; fi
